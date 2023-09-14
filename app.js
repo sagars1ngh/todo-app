c620aaf80a7c247bd331ccf0c845ca999fd25c51
sagars1ngh <143807975+sagars1ngh@users.noreply.github.com>
2023-09-14 22:33:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            newTask: '',
+            tasks: [],
+        };
+    },
+    methods: {
+        addTask() {
+            if (this.newTask.trim() !== '') {
+                this.tasks.push({ text: this.newTask, completed: false });
+                this.newTask = '';
+            }
+        },
+        removeTask(index) {
+            this.tasks.splice(index, 1);
+        },
+        clearCompleted() {
+            this.tasks = this.tasks.filter((task) => !task.completed);
+        },
+    },
+});
+
+app.mount('#app');
